72933b98ab8d1b015e13475907c94d5b718f4ffb
motjuste <dada3mn@gmail.com>
2015-11-24 20:43:56 +0100

    changed the logic; inspired by ck
    more mathmatically correct
    and of course faster!

--- pattrex/unit_circles.py
+++ pattrex/unit_circles.py
@@ -8,6 +8,37 @@ import matplotlib.pyplot as plt
 
 
 def plot_Lp_unit_circle(p=2, atol=0.001):
+    x = np.linspace(0, 1, 2000)
+
+    y = np.power((1 - np.power(x, p)), 1/p)
+
+    # plot the results
+    plt.figure()
+    plt.plot(x, y)
+
+    # since the unit circle is symmetric in all quadrants
+    # the plots in different quadrants would appear in different colors
+    # this is intentional
+    plt.plot(-x, y)
+    plt.plot(x, -y)
+    plt.plot(-x, -y)
+
+    # set title of plot to show p
+    plt.title("p=%.1f" % (p))
+
+    # make the plot aspect ratio square and set range
+    plt.axis('equal')
+    plt.axis([-1.2, 1.2, -1.2, 1.2])
+
+    # add axes through the origin and grids
+    plt.axhline(y=0)
+    plt.axvline(x=0)
+    plt.grid()
+
+    plt.show()
+
+
+def plot_Lp_unit_circle_linalg(p=2, atol=0.001):
     x = np.linspace(-1, 1, 2000)
     y = np.linspace(-1, 1, 2000)
 
