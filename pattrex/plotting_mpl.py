b9c3f52e1a0a1f2592d92c7ff5400b2c8e36de04
motjuste <dada3mn@gmail.com>
2015-12-07 00:04:31 +0100

    Allow using own figure and axes

--- pattrex/plotting_mpl.py
+++ pattrex/plotting_mpl.py
@@ -5,16 +5,17 @@ Basic plotting using matplotlib
 import matplotlib.pyplot as plt
 
 
-def plot2d(X, show=True, set_aspect_equal=True, x_lim=None, y_lim=None, show_axes_through_origin=False, plotlabel=None, title=None):
+def plot2d(X, axs=None, show=True, set_aspect_equal=True, x_lim=None, y_lim=None, show_axes_through_origin=False, plotlabel=None, title=None):
 
     # FIXME: @motjuste: proper asserts, and testing, please
 
-    # create a new figure and axes
-    fig = plt.figure()
-    axs = fig.add_subplot(111)
+    if axs is None:
+        # create a new figure and axes
+        fig = plt.figure()
+        axs = fig.add_subplot(111)
 
     # meat and potato of plotting
-    axs.plot(X[0,:], X[1, :], 'ro', label=plotlabel)
+    axs.plot(X[0,:], X[1, :], 'o', label=plotlabel)
 
     # Garnishing starts here
     if title is not None:
