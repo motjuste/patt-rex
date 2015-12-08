b86ebf6c247c45a06d8d15967e766b616fc17392
motjuste <dada3mn@gmail.com>
2015-12-08 22:32:14 +0100

    task 1.5 using Sieve Approach

--- pattrex/unit_circles.py
+++ pattrex/unit_circles.py
@@ -12,3 +12,15 @@ def lp_unit_circle(p=2):
     y = np.power((1 - np.power(x, p)), 1/p)
 
     return (x, y)
+
+
+def aitchison_norm(xyz):
+    lxyz = np.log(xyz)
+    lgxyz = np.mean(lxyz, axis=0)
+
+    # test show tiling not required
+#     if len(lxyz.shape) > 1:
+#         lgxyz = np.tile(lgxyz, (lxyz.shape[0], 1, 1, 1))
+
+    diff = lxyz - lgxyz
+    return np.sqrt(np.sum(diff * diff, axis=0))
